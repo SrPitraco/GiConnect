254585dd4563a19db6516157b1d7a2d246f0a15b
SrPitraco <vandamni@hotmail.com>
2025-05-20 22:54:26 +0200

    Algunas pages

--- GiConnect/src/app/app.routes.ts
+++ GiConnect/src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./auth/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'list',
+    loadComponent: () => import('./clases/list/list.page').then( m => m.ListPage)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./maestro/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
 ];
